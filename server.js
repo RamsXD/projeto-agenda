ebba9d9c74b293ce043c2dde1e552b8b4a5c043c
Alessandro Ramiro <ramsxd67@gmail.com>
2025-05-27 17:43:00 -0300

    final

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect(process.env.CONNECTIONSTRING, 
+  { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  })
+
   .then(() => {
     app.emit('pronto');
   })
@@ -10,11 +16,15 @@ mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifi
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
-const routes = require('./routes');
+
+
 const path = require('path');
 const helmet = require('helmet');
 const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
+
+const routes = require('./routes');
+
 
 app.use(helmet());
 
